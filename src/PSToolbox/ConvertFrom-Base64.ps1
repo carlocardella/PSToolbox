3e732003f00c18f9f20288ad81de150056fdc907
Carlo Cardella <carloc@microsoft.com>
2020-06-15 17:43:12 -0700

    add first tests

---
+++ src/PSToolbox/ConvertFrom-Base64.ps1
@@ -0,0 +1,29 @@
+﻿function ConvertFrom-Base64 {
+    <#
+    .SYNOPSIS
+    Converts a Base64 string to its original value
+    
+    .PARAMETER Value
+    The Base64 string to convert
+    
+    .PARAMETER Encoding
+    Optional Encoding to sue; default: Unicode
+    
+    .EXAMPLE
+    ConvertFrom-Base64 -Value $myObject
+    #>
+    param(
+        [parameter(mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string[]]$Value,
+
+        [parameter()]
+        [System.Text.Encoding]$Encoding = [System.Text.Encoding]::Unicode
+    )
+
+    process {
+        foreach ($item in $Value) {
+            $bytes = [System.Convert]::FromBase64String($item)
+            $Encoding.GetString($bytes)
+        }
+    }
+}
