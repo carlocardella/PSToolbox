1372f19981f9da97b51d4b1946a61ee38084d01f
Carlo Cardella <carloc@microsoft.com>
2020-06-15 17:31:49 -0700

    import functions from other modules

---
+++ src/PSToolbox/Convert-CertBase64DataToFile.ps1
@@ -0,0 +1,57 @@
+﻿function Convert-CertBase64DataToFile {
+    <#
+    .SYNOPSIS
+    Converts the Base64 string representation of a certificate to a Certificate object and optionally
+    saves the certificate to disk
+
+    .PARAMETER CertBase64Data
+    Base64 String certificate representation
+
+    .PARAMETER OutputFile
+    Full path to the certificate to save to disk
+
+    .PARAMETER Password
+    Password to protect the private key certificate (.pfx)
+
+    .EXAMPLE
+    Convert-CertBase64DataToFile -CertBase64Data (Get-AzKeyVaultSecret myCertificateSecret).SecretValueText -OutputFile C:\LocalTemp\myCertificateSecret.pfx -Password (Read-Host -AsSecureString p)
+    p: *************
+    #>
+
+    param(
+        [parameter(Mandatory, Position = 1, ValueFromPipeline)]
+        ## Base64String representation of the certificate object
+        [Alias('Data')]
+        [string]$CertBase64Data,
+
+        [parameter(Position = 2)]
+        ## Full file path where to save the converted certificate
+        [string]$OutputFile,
+
+        [parameter(Position = 3)]
+        ## password to use to save a Private Key file
+        [securestring]$Password
+    )
+
+    process {
+        [byte[]]$certData = [System.Convert]::FromBase64String($CertBase64Data)
+        $certObject = [System.Security.Cryptography.X509Certificates.X509Certificate2] $certData
+
+        if (! [string]::IsNullOrWhiteSpace($OutputFile)) {
+            if (! [string]::IsNullOrWhiteSpace($Password)) {
+                if (! $certObject.HasPrivateKey) {
+                    Write-Warning "Saving a .cer file in .pfx format with password may result in a corrupted file"
+                }
+                $certCollection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection
+                $certCollection.Import($certData, $null, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
+                [System.IO.File]::WriteAllBytes($OutputFile, $certCollection.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $Password))
+            }
+            else {
+                [System.IO.File]::WriteAllBytes($OutputFile, $certObject.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
+            }
+        }
+        else {
+            $certObject
+        }
+    }
+}
---
+++ src/PSToolbox/Update-StringInFile.ps1
@@ -0,0 +1,87 @@
+﻿function Update-StringInFile {
+    <#
+    .SYNOPSIS
+    Updates (replaces) all occurrences of a given string in a file with a new value; the file is overwritten in place
+
+    .PARAMETER FilePath
+    Path to the file to update
+
+    .PARAMETER OldString
+    String to replace with $NewString
+
+    .PARAMETER NewString
+    New string to use as replacement for $OldString
+
+    .PARAMETER MatchCase
+    Treat $OldString as case sensitive
+
+    .PARAMETER Force
+    Suppress confirmation prompt
+
+    .EXAMPLE
+    Update-StringInFile -FilePath c:\temp\myfile.txt -OldString 'replace me!' -NewString 'new value'
+
+    This command replaces all occurrences of OldString with NewString in c:\temp\myfile.txt
+
+    .EXAMPLE
+    dir c:\temp\*.txt | Update-StringInFile -OldString 'replace me!' -NewString 'new value'
+
+    This command replaces OldString with NewString in all files received from the pipeline
+    #>
+    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'outputfolder')]
+    param(
+        [parameter(Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias('Fullname', 'Path')]
+        [ValidateScript( { if (!(Test-Path -Path $_)) { throw "Invalid path: $_" } })]
+        [ValidateScript( { if (!(Test-Path -Path $_ -PathType 'Leaf')) { throw "FilePath must be a file: $_" } })]
+        [string[]]$FilePath,
+
+        [parameter(Mandatory, Position = 1)]
+        [string]$OldString,
+
+        [parameter(Mandatory, Position = 2)]
+        [string]$NewString,
+
+        [parameter()]
+        [switch]$MatchCase,
+
+        [parameter()]
+        [switch]$Force
+    )
+
+    process {
+        foreach ($file in $FilePath) {
+            Write-Verbose -Message "Working on $file"
+            [bool]$updateFile = $false
+
+            if (Select-String -Path $file -Pattern $OldString -CaseSensitive:$MatchCase -SimpleMatch) {
+                Write-Verbose "Match found"
+                $updateFile = $true
+            }
+            else {
+                Write-Verbose "No match found, skipping"
+                continue
+            }
+
+            $newContent = $null
+            $oldContent = Get-Content -Path $file
+
+            if ($MatchCase) {
+                $newContent = $oldContent.Replace($OldString, $NewString)
+            }
+            else {
+                $newContent = $oldContent | ForEach-Object { $_.replace($OldString, $NewString) }
+            }
+
+            if ($updateFile -and $PSCmdlet.ShouldProcess("$file", "Overwrite file")) {
+                if ($updateFile -and ($Force -or ($PSCmdlet.ShouldContinue("Overwrite $file?", "Overwriting $file")))) {
+                    Write-Verbose -Message "Updating $file"
+                    Set-Content -Path $file -Value $newContent -Force
+                }
+                else {
+                    $newContent
+                }
+            }
+        }
+    }
+}
