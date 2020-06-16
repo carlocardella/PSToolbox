93b2906452f1f8f89eb11d0b3b4ddfb37b77e88f
Carlo Cardella <carloc@microsoft.com>
2020-06-15 17:44:12 -0700

    add experimental functions

---
+++ src/PSToolbox/ConvertFrom-PsCustomObjectToHashtable.ps1
@@ -0,0 +1,33 @@
+﻿function ConvertFrom-PsCustomObjectToHashtable {
+    <#
+    .SYNOPSIS
+        Converts the passed object to an hashtable
+
+    .EXAMPLE
+        ConvertFrom-PsCustomObjectToHashtable -PsObject $mypsobject
+    #>
+    [CmdletBinding()]
+    [OutputType([hashtable[]])]
+    param(
+        [parameter(mandatory = $true, position = 1, ValueFromPipeline = $true)]
+        [PsObject]$PsObject
+    )
+
+    Write-Verbose -Message "[Extracting object properties]"
+    $propertyNames = $PsObject | Select-Object -Property * -First 1 | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
+
+    $outObj = @()
+
+    Write-Verbose -Message "[Looping through all object entries]"
+    foreach ($object in $PsObject) {
+        $ht = @{ }
+
+        foreach ($name in $propertyNames) {
+            $ht.Add($name, $object.$name)
+        }
+
+        $outObj += $ht
+    }
+
+    $outObj
+}
---
+++ src/PSToolbox/Set-WindowTitle.ps1
@@ -0,0 +1,17 @@
+﻿function Set-WindowTitle {
+    <#
+	.SYNOPSIS
+	Sets a custom title Powershell console window
+
+	.PARAMETER Title
+	Title to set
+
+	.EXAMPLE
+	Set-WindowTitle -Title
+	#>
+    param(
+        [String]$title = ''
+    )
+
+    $Host.UI.RawUI.WindowTitle = $title
+}
