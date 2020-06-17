da08261167b2d895b95fc391c9f49f2a449591b1
Carlo Cardella <carloc@microsoft.com>
2020-06-17 15:27:32 -0700

    add functions from private repo

---
+++ src/PSToolbox/Get-WindowTitle.ps1
@@ -0,0 +1,14 @@
+﻿function Get-WindowTitle {
+	<#
+	.SYNOPSIS
+	Gets a custom title Powershell console window
+
+	.EXAMPLE
+	Get-WindowTitle
+	
+	C:\Program Files\PowerShell\7\pwsh.exe
+	#>
+	param()
+
+	$Host.UI.RawUI.WindowTitle
+}
