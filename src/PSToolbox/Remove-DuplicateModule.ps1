7c106ee42305f39ee799fac0efb1100726ccefda
Carlo Cardella <carloc@microsoft.com>
2020-07-05 09:21:22 -0700

    - folder acceps an array of paths - use workaround due to a bug removing items in OneDrive or under a Junction - add inline help and examples

--- src/PSToolbox/Remove-DuplicateModule.ps1
+++ src/PSToolbox/Remove-DuplicateModule.ps1
@@ -1,26 +1,108 @@
 function Remove-DuplicateModule {
+    <#
+    .SYNOPSIS
+    Removes outdated module versions and keeps the most recent one
+
+    .DESCRIPTION
+    Over time you may be in a situation where multiple, oudated versins of the same module are installed on the machine, for example:
+
+        Get-Module az.* -ListAvailable
+
+            Directory: C:\Users\carlo\Documents\PowerShell\Modules
+
+        ModuleType Version    PreRelease Name                                PSEdition ExportedCommands
+        ---------- -------    ---------- ----                                --------- ----------------
+        Script     1.8.1                 Az.Accounts                         Core,Desk {Disable-AzDataCollection, Disable-AzContextAutosave, Enable-AzDataCollection, Enable-AzContextAutosa…
+        Script     1.8.0                 Az.Accounts                         Core,Desk {Disable-AzDataCollection, Disable-AzContextAutosave, Enable-AzDataCollection, Enable-AzContextAutosa…
+        Script     1.7.5                 Az.Accounts                         Core,Desk {Disable-AzDataCollection, Disable-AzContextAutosave, Enable-AzDataCollection, Enable-AzContextAutosa…
+        Script     1.1.1                 Az.Advisor                          Core,Desk {Get-AzAdvisorRecommendation, Enable-AzAdvisorRecommendation, Disable-AzAdvisorRecommendation, Get-Az…
+        Script     1.1.1                 Az.Aks                              Core,Desk {Get-AzAks, New-AzAks, Remove-AzAks, Import-AzAksCredential…}
+        Script     1.0.3                 Az.Aks                              Core,Desk {Get-AzAks, New-AzAks, Remove-AzAks, Import-AzAksCredential…}
+        Script     0.1.2                 Az.AlertsManagement                 Core,Desk {Get-AzAlert, Get-AzAlertObjectHistory, Update-AzAlertState, Measure-AzAlertStatistic…}
+        Script     0.1.1                 Az.AlertsManagement                 Core,Desk {Get-AzAlert, Get-AzAlertObjectHistory, Update-AzAlertState, Measure-AzAlertStatistic…}
+        Script     1.1.3                 Az.AnalysisServices                 Core,Desk {Resume-AzAnalysisServicesServer, Suspend-AzAnalysisServicesServer, Get-AzAnalysisServicesServer, Rem…
+        Script     1.1.2                 Az.AnalysisServices                 Core,Desk {Resume-AzAnalysisServicesServer, Suspend-AzAnalysisServicesServer, Get-AzAnalysisServicesServer, Rem…
+        Script     2.0.1                 Az.ApiManagement                    Core,Desk {Add-AzApiManagementApiToProduct, Add-AzApiManagementProductToGroup, Add-AzApiManagementRegion, Add-A…
+        Script     2.0.0                 Az.ApiManagement                    Core,Desk {Add-AzApiManagementApiToProduct, Add-AzApiManagementProductToGroup, Add-AzApiManagementRegion, Add-A…
+        Script     1.4.1                 Az.ApiManagement                    Core,Desk {Add-AzApiManagementApiToProduct, Add-AzApiManagementProductToGroup, Add-AzApiManagementRegion, Add-A…
+        Script     0.1.4                 Az.AppConfiguration                 Core,Desk {Get-AzAppConfigurationStore, Get-AzAppConfigurationStoreKey, Get-AzAppConfigurationStoreKeyValue, Ne…
+        Script     1.1.0                 Az.ApplicationInsights              Core,Desk {Get-AzApplicationInsights, New-AzApplicationInsights, Remove-AzApplicationInsights, Update-AzApplica…
+        Script     1.0.3                 Az.ApplicationInsights              Core,Desk {Get-AzApplicationInsights, New-AzApplicationInsights, Remove-AzApplicationInsights, Set-AzApplicatio…
+        Binary     1.0.1                 Az.ApplicationMonitor               Desk
+        Script     0.1.7                 Az.Attestation                      Core,Desk {New-AzAttestation, Get-AzAttestation, Remove-AzAttestation, Get-AzAttestationPolicy…}
+        Script     1.3.6                 Az.Automation                       Core,Desk {Get-AzAutomationHybridWorkerGroup, Remove-AzAutomationHybridWorkerGroup, Get-AzAutomationJobOutputRe…
+        Script     3.0.0                 Az.Batch                            Core,Desk {Remove-AzBatchAccount, Get-AzBatchAccount, Get-AzBatchAccountKey, New-AzBatchAccount…}
+        Script     2.0.2                 Az.Batch                            Core,Desk {Remove-AzBatchAccount, Get-AzBatchAccount, Get-AzBatchAccountKey, New-AzBatchAccount…}
+        Script     1.0.3                 Az.Billing                          Core,Desk {Get-AzBillingInvoice, Get-AzBillingPeriod, Get-AzEnrollmentAccount, Get-AzConsumptionBudget…}
+        Script     1.0.2                 Az.Billing                          Core,Desk {Get-AzBillingInvoice, Get-AzBillingPeriod, Get-AzEnrollmentAccount, Get-AzConsumptionBudget…}
+        Script     0.2.13                Az.Blueprint                        Core,Desk {Get-AzBlueprint, Get-AzBlueprintAssignment, New-AzBlueprintAssignment, Remove-AzBlueprintAssignment…}
+        Script     0.2.12                Az.Blueprint                        Core,Desk {Get-AzBlueprint, Get-AzBlueprintAssignment, New-AzBlueprintAssignment, Remove-AzBlueprintAssignment…}
+        [...]
+
+    Removing all those outdated modules can be tedious and time consuming, this function automates the task.
+    By default the function uses 'Get-Module -ListAvailable' to retrieve the list of modules available in $env:PSModulePath, but you can specify a list of folders to scan instead of searching all available locations. 
+
+    .PARAMETER Folder
+    Full path to the folder(s) you want to scan for duplicate modules
+
+    .PARAMETER Force
+    Suppresses the confirmation prompt when removing old module
+
+    .EXAMPLE
+    Remove-DuplicateModule -WhatIf
+
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.Accounts\1.7.5".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.Accounts\1.8.0".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.Aks\1.0.3".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.AlertsManagement\0.1.1".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.AnalysisServices\1.1.2".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.ApiManagement\1.4.1".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.ApiManagement\2.0.0".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.ApplicationInsights\1.0.3".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.Batch\2.0.2".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.Billing\1.0.2".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.Compute\3.7.0".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.DataFactory\1.7.0".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.DataFactory\1.8.0".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\PowerShell\Modules\Az.DataShare\0.1.2".
+
+    .EXAMPLE
+    Remove-DuplicateModule 'C:\Users\myuser\Documents\Powershell\Modules\Az.Blueprint','C:\Users\myuser\Documents\Powershell\Modules\Az.CognitiveServices' -WhatIf
+
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\Powershell\Modules\Az.Blueprint\0.2.12".
+    What if: Performing the operation "Remove module" on target "C:\Users\myuser\Documents\Powershell\Modules\Az.CognitiveServices\1.3.0".
+
+    .EXAMPLE
+    Remove-DuplicateModule 'C:\Users\myuser\Documents\Powershell\Modules\Az.Blueprint','C:\Users\myuser\Documents\Powershell\Modules\Az.CognitiveServices' -Verbose -Force
+
+    VERBOSE: Az.Blueprint, 0.2.12
+    VERBOSE: Az.CognitiveServices, 1.3.0
+    #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param (
         [parameter(Position = 0)]
         [ValidateScript( { Test-Path $_ })]
-        [string]$ModuleFolder
+        [string[]]$Folder,
+
+        [parameter()]
+        [switch]$Force
     )
 
-    if (! [string]::IsNullOrWhiteSpace($ModuleFolder)) {
-        $groups = Get-Module -ListAvailable | Where-Object Path -EQ $ModuleFolder | Group-Object 'Name' | Where-Object Count -gt 1
+    if (! [string]::IsNullOrWhiteSpace($Folder)) {
+        $groups = Get-ChildItem -Path $Folder -Recurse -Filter '*.psd1' | ForEach-Object {
+            Test-ModuleManifest -Path $_.FullName -ErrorAction 'SilentlyContinue' -Verbose:$false
+        } | Group-Object 'Name' | Where-Object 'Count' -GT 1
     }
     else {
-        $groups = Get-Module -ListAvailable | Where-Object Path -Like "$env:USERPROFILE*" | Group-Object 'Name' | Where-Object Count -gt 1
+        $groups = Get-Module -ListAvailable -Verbose:$false | Where-Object Path -Like "$env:USERPROFILE*" | Group-Object 'Name' | Where-Object 'Count' -GT 1
     }
-    
+
     foreach ($group in $groups) {
         $versions = $null
         $versions = $group.Group | Sort-Object 'Version' | Select-Object -SkipLast 1
         $versionToKeep = $null
         $versionToKeep = $group.Group | Sort-Object 'Version' | Select-Object -Last 1
 
-        Write-Verbose "$($versionToKeep.Name), $($versionToKeep.Version)"
-    
         foreach ($version in $versions) {
             Write-Verbose "$($version.Name), $($version.Version)"
 
@@ -28,7 +110,10 @@ function Remove-DuplicateModule {
             $moduleFolder = Split-Path $version.Path -Parent
             if ($Force -or ($PSCmdlet.ShouldProcess("$moduleFolder", "Remove module"))) {
                 if ($Force -or ($PSCmdlet.ShouldContinue("Remove module?", "$moduleFolder"))) {
-                    Remove-Item -Path $moduleFolder -Force -Recurse
+                    # todo: workaround for https://github.com/PowerShell/PowerShell/issues/9246 (https://github.com/PowerShell/PowerShell/issues/11721)
+                    # todo: this does not address the problem if the path is a subfolder of a junction
+                    Get-ChildItem -Path $moduleFolder -Recurse -Force | Remove-Item -Force
+                    Remove-Item -Path $moduleFolder -Force
                 }
             }
         }
