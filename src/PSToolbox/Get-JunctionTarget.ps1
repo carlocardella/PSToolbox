f33f51f59376d0734edec6e26caa445106ab7a98
Carlo Cardella <carloc@microsoft.com>
2020-07-07 21:51:14 -0700

    work on junction function

--- src/PSToolbox/Get-JunctionTarget.ps1
+++ src/PSToolbox/Get-JunctionTarget.ps1
@@ -3,18 +3,39 @@ function Get-JunctionTarget {
     param (
         [parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
         [ValidateScript( { Test-Path -Path $_ })]
+        [ValidateScript( { if (!(Split-Path -Path $_ -Qualifier)) { 
+                    throw [System.ArgumentException]::new("Invalid path provided, must be a local drive")
+                }
+                $true
+            }
+        )]
         [Alias('FullName')]
         [string]$Path
     )
 
     process {
         # get the path tokens but skip the Qualifier (drive root)
-        $pathTockens = $Path -split '\\' #| Select-Object * -Skip 1
+        # $pathTokens = $Path -split '\\' | Select-Object -Skip 1
+        $pathTokens = $Path -split '\\' #| Select-Object -Skip 1
         $pathToTest = Split-Path -Path $Path -Qualifier
 
-        foreach ($token in $pathTockens) {
-            $pathToTest = Join-Path -Path $pathToTest -ChildPath $token
-            $pathToTest
+        # foreach ($token in $pathTokens) {
+        #     $pathToTest = Join-Path -Path $pathToTest -ChildPath $token
+        #     $item = Get-Item -Path $pathToTest
+        #     if ($item.LinkType -eq 'Junction') {
+        #         $item
+        #         break
+        #     }
+        # }
+        for ($i = 1; $i -le $pathTokens.Length; $i++) {
+            $pathToTest = Join-Path $pathToTest -ChildPath $pathTokens[$i]
+            $item = Get-Item -Path $pathToTest
+            if ($item.LinkType -eq 'Junction') {
+                # $item
+                $junctionTaget = Join-Path -Path $item.Target -ChildPath ($pathTokens[$i..100] -join '\')
+                $junctionTaget
+                break
+            }
         }
     }
 }
